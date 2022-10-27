0855d13f83c116abbbf1ac1eddec89a149b18071
Julio Cesar Bueno <buenojc@outlook.com>
2022-10-27 17:17:43 -0300

    Adiciona função listar

--- services/UsuariosServices.js
+++ services/UsuariosServices.js
@@ -1,63 +1,77 @@
-function listar(){
-    // Seu código aqui
+const usuarios = require("../databases/usuarios.json");
+
+function listar() {
+  console.table(
+    usuarios.map( usuario => {
+      return {
+        id: usuario.id,
+        nome: usuario.nome,
+        email: usuario.email,
+      };
+    })
+  );
 }
 
-function salvar(arrayDeUsuarios){
-    // Seu código aqui
+function salvar(arrayDeUsuarios) {
+  // Seu código aqui
 }
 
-function cadastrar(objeto){
-// Seu código aqui
+function cadastrar(objeto) {
+  // Seu código aqui
 }
 
-function detalhar(idUsuario){
-// Seu código aqui
+function detalhar(idUsuario) {
+  // Seu código aqui
 }
 
-function remover(idDoUsuarioParaRemover){
-    // Seu código aqui
+function remover(idDoUsuarioParaRemover) {
+  // Seu código aqui
 }
 
-function alterar(novosDados, idUsuario){
-    // Seu código aqui
+function alterar(novosDados, idUsuario) {
+  // Seu código aqui
 }
 
-function addEndereco(novoEndereco, idUsuario){
-    // Seu código aqui
+function addEndereco(novoEndereco, idUsuario) {
+  // Seu código aqui
 }
 
-function removerEndereco(posicaoDoEndereco, idUsuario){
-// Seu código aqui
+function removerEndereco(posicaoDoEndereco, idUsuario) {
+  // Seu código aqui
 }
 
-function alterarEndereco(posicaoDoEndereco, novoEndereco, idUsuario){
-// Seu código aqui        
+function alterarEndereco(posicaoDoEndereco, novoEndereco, idUsuario) {
+  // Seu código aqui
 }
 
-function addFormaDePagamento(novaFormaDePagamento, idUsuario){
-    // Seu código aqui
+function addFormaDePagamento(novaFormaDePagamento, idUsuario) {
+  // Seu código aqui
 }
 
-function removerFormaDePagamento(posicaoDaFormaDePagamento, idUsuario){
-    // Seu código aqui
+function removerFormaDePagamento(posicaoDaFormaDePagamento, idUsuario) {
+  // Seu código aqui
 }
 
-function alterarFormaDePagamento(novaFormaDePagamento, posicaoDaFormaDePagamento, idUsuario){
-    // Seu código aqui
+function alterarFormaDePagamento(
+  novaFormaDePagamento,
+  posicaoDaFormaDePagamento,
+  idUsuario
+) {
+  // Seu código aqui
 }
 
 const UsuariosServices = {
-    cadastrar,
-    listar,
-    detalhar,
-    remover,
-    alterar,
-    addEndereco,
-    removerEndereco,
-    alteraEndereco: alterarEndereco,
-    addFormaDePagamento,
-    removerFormaDePagamento,
-    alterarFormaDePagamento
-}
+  cadastrar,
+  listar,
+  detalhar,
+  remover,
+  alterar,
+  addEndereco,
+  removerEndereco,
+  alteraEndereco: alterarEndereco,
+  addFormaDePagamento,
+  removerFormaDePagamento,
+  alterarFormaDePagamento,
+};
 
 module.exports = UsuariosServices;
