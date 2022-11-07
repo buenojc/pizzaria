69ae9add9a20454ef7c141c2e1a994405aa0cc99
Julio Cesar Bueno <buenojc@outlook.com>
2022-11-07 11:34:07 -0300

    Cria views e envia arquivo html na resposta da requisição

--- app.js
+++ app.js
@@ -4,7 +4,8 @@ const servidor = express();
 
 servidor.get('/usuarios', (req, res) => {
     console.log("chegou uma requisição!")
-    return res.send('Enviando lista de usuários')
+    // return res.send('Enviando lista de usuários')
+    return res.sendFile(__dirname + '/views/index.html')
 })
 
 
