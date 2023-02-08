7ae8b9fa0418fb97b4a8dda73796f6d83806202b
Julio Cesar Bueno <buenojc@outlook.com>
2023-02-08 10:43:56 -0300

    feat: pega dados do usuário dinamicamente

--- controllers/PaginasController.js
+++ controllers/PaginasController.js
@@ -1,21 +1,31 @@
-const path = require("path");
 const pizzas = require("../databases/pizzas.json");
+const idu = 1;
 
 const PaginasController = {
   showIndex: (req, res) => {
     return res.render("index");
   },
+
   showCarrinho: (req, res) => {
     const carrinho = [pizzas[0], pizzas[1], pizzas[2]];
-    const nomeDoUsuario = "Ligia Pretel"
+    const nomeDoUsuario = "Ligia Pretel";
     return res.render("carrinho", { carrinho, nomeDoUsuario });
   },
+
   showPerfil: (req, res) => {
-    return res.render("perfil");
+    const usuarios = require("../databases/usuarios.json");
+    const usuario = usuarios.find((usuario) => usuario.id == idu);
+    if (usuario !== undefined) {
+      return res.render("perfil", { usuario: usuario });
+    } else {
+      return res.render("erro-404");
+    }
   },
+
   showCadastro: (req, res) => {
     return res.render("cadastro");
   },
+
   showPizza: (req, res) => {
     const id = req.params.id;
     const pizza = pizzas.find((pizza) => pizza.id == id);
