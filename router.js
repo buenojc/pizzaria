04f7a29b9bb968cf55689a30f01ae4cfcca31b02
Julio Cesar Bueno <buenojc@outlook.com>
2023-02-13 11:34:07 -0300

    add: rotas adm

--- router.js
+++ router.js
@@ -3,20 +3,24 @@ const express = require("express");
 
 // Importar o controller
 const PaginasController = require("./controllers/PaginasController");
+const AdmController = require("./controllers/AdmController")
 
 // Criar o roteador
 const router = express.Router();
 
 // Definir as rotas para o roteador
 router.get("/", PaginasController.showIndex);
-
 router.get("/carrinho", PaginasController.showCarrinho);
-
 router.get("/perfil", PaginasController.showPerfil);
-
 router.get("/cadastro", PaginasController.showCadastro);
+router.get("/pizzas/:id", PaginasController.showPizza);
 
-router.get("/pizzas/:id", PaginasController.showPizza)
+router.get("/adm/pizzas", AdmController.listarPizzas);
+router.get("/adm/pizzas/create", AdmController.criarPizza);
+router.get("/adm/pizzas/edit", (req, res) => {});
+router.post("/adm/pizzas/store", AdmController.gravarPizza);
+router.post("/adm/pizzas/update", (req, res) => {});
+router.post("/adm/pizzas/delete", (req, res) => {})
 
 // Exportar o roteador
 module.exports = router;
