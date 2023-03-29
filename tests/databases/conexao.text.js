a735c6117e7a5cb3a358fc9abf954cdcdbb17653
Julio Cesar Bueno <buenojc@outlook.com>
2023-03-29 11:22:18 -0300

    add: configuração e conexão com bd

---
+++ tests/databases/conexao.text.js
@@ -0,0 +1,28 @@
+// importando o sequelize
+const sequelize = require("sequelize");
+
+// importar configurações
+const config = require("../../databases/config").development;
+
+// Criar a conexão
+const conexao = new sequelize(config);
+
+// Executar consulta
+const sql = `select 
+	p.id, p.nome, 
+    ifnull(sum(pp.quantidade), 0) as pizzas_pedidas 
+from 
+	pizzas as p
+	left join pedido_pizza as pp on pp.pizza_id = p.id
+group by 
+	p.id, p.nome
+order by 
+	sum(pp.quantidade) DESC;`;
+
+const promessa = conexao.query(sql)
+
+promessa.then(
+    dados => {
+        console.log(dados)
+    }
+)
