c1d3dd93214ffa027bd269382a5d8d2c468bfb4c
Julio Cesar Bueno <buenojc@outlook.com>
2023-04-05 09:19:24 -0300

    fix: condição de busca de teste

--- tests/databases/Pizza.test.js
+++ tests/databases/Pizza.test.js
@@ -3,7 +3,7 @@ const { Pizza } = require('../../databases/models')
 async function buscaPizzas() {
 
     // raw traz só os dados do banco de dados ao invez de funções junto com dados
-    let pizzas = await Pizza.findAll({attribute: {exclude: ['nome']}, raw: true})
+    let pizzas = await Pizza.findAll({raw:true})
     console.log(pizzas)
 }
 
